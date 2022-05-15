3cc633fb850d22bb3a4b440e78c2863901f5f0d3
Austin White <austinlldb@gmail.com>
2022-05-14 20:02:19 -0600

    redirect md5sum output to /dev/null

--- backup.sh
+++ backup.sh
@@ -2,9 +2,8 @@
 
 BOOKMARKS_PATH=~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks
 SUBMODULE=bookmarks
-SECONDS_IN_A_DAY=86400
 
-suffix=`date +%m%d%y_%H%M%S`
+suffix=$(date +%m%d%y_%H%M%S)
 fileName=Bookmarks_${suffix}.json
 backupFilePath=$SUBMODULE/$fileName
 md5CheckSum=lastbackup
@@ -16,7 +15,7 @@ copyBookmarks() {
 
 validateCheckSum() {
     if [ -f "$md5CheckSum" ]; then
-        md5sum -c $md5CheckSum
+        md5sum -c $md5CheckSum > /dev/null 2>&1
         if [ $? -ne 0 ]; then
             md5sum $BOOKMARKS_PATH > $md5CheckSum
         else
@@ -32,20 +31,16 @@ validateCheckSum() {
 checkLastBackup() {
     validateCheckSum
     if [ $? -ne 0 ]; then
+        echo "exiting: no changes to 'Bookmarks' file."
         exit 1
-    fi
-
-    bookmarkTime=`stat -c %Y $BOOKMARKS_PATH`
-    currentTime=`date +%s`
-    timeDifference=`expr $currentTime - $bookmarkTime`
- 
-    if [ $timeDifference -lt $SECONDS_IN_A_DAY ]; then
+    else
+        echo "changes to 'Bookmarks' file detected. performing backup..."
         copyBookmarks
     fi
 }
 
 pushBackup() {
-    commitMessage="Backup: `date "+%b %d, %Y"`"
+    commitMessage="Backup: $(date "+%b %d, %Y")"
 
     cd $SUBMODULE
     git checkout main
@@ -63,15 +58,18 @@ usage() {
     echo "    run-type: force, check (default: check)"
 }
 
+
 if [ "$#" -gt 1 ]; then
     echo "error: to many peremeters"
     usage
     exit 1
 fi
 
+
 if [ -n "$1" ]; then
     runType=$1
 fi
+
 
 if [ $runType == "force" ]; then
     copyBookmarks
@@ -83,5 +81,5 @@ else
     exit 1
 fi
 
-pushBackup
 
+pushBackup
